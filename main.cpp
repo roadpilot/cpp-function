04d4e962b5f7da06a04a2e974187edef19a62e5d
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-11-07 23:34:00 +0000

    stub

--- main.cpp
+++ main.cpp
@@ -1,2 +1,18 @@
 //Write a function int max_of_four(int a, int b, int c, int d) which returns the maximum of the four arguments it receives.
 
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
